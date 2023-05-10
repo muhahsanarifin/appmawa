d882555b2ab76ef3aba5bbfa11c9a2347ad2b1f3
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-11 01:51:22 +0700

    Add router

---
+++ src/router.tsx
@@ -0,0 +1,24 @@
+import Home from "./pages/home";
+import Result from "./pages/result";
+import Title from "./components/TitleBar";
+
+const router = [
+  {
+    path: "/",
+    element: (
+      <Title onTitle="Home">
+        <Home />
+      </Title>
+    ),
+  },
+  {
+    path: "/result",
+    element: (
+      <Title onTitle="Result">
+        <Result />
+      </Title>
+    ),
+  },
+];
+
+export default router;
