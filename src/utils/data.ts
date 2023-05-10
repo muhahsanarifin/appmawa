b63658287e8077abe9bc766a06c9b3b8c14a6622
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-11 01:52:38 +0700

    Add utils

---
+++ src/utils/data.ts
@@ -0,0 +1,44 @@
+const jumlah_mahasiswa = [
+  {
+    id: 1,
+    name: "Mahasiswa 1",
+  },
+  {
+    id: 2,
+    name: "Mahasiswa 2",
+  },
+  {
+    id: 3,
+    name: "Mahasiswa 3",
+  },
+  {
+    id: 4,
+    name: "Mahasiswa 4",
+  },
+  {
+    id: 5,
+    name: "Mahasiswa 5",
+  },
+  {
+    id: 6,
+    name: "Mahasiswa 6",
+  },
+  {
+    id: 7,
+    name: "Mahasiswa 7",
+  },
+  {
+    id: 8,
+    name: "Mahasiswa 8",
+  },
+  {
+    id: 9,
+    name: "Mahasiswa 9",
+  },
+  {
+    id: 10,
+    name: "Mahasiswa 10",
+  },
+];
+
+export default jumlah_mahasiswa;
---
+++ src/utils/types/components.ts
@@ -0,0 +1,11 @@
+export type SaveButtonProps = {
+  initial?: string;
+  onBody?: any;
+  onAction?: any;
+};
+
+export type RegulerButtonProps = {
+  initial?: string;
+  onClick?: any;
+  onStyle?: string;
+};
