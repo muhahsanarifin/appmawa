05aac99650d923e32c72ec6da0d02b2856fd4be2
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-07-02 15:44:25 +0700

    Update TitleBar.tsx

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,12 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useTitle } from "react-use";
 
 const Title: React.FC<{ onTitle: any; children: React.ReactNode }> = ({
   onTitle,
   children,
 }) => {
-  useEffect(() => {
-    document.title = `Appmawa | ${onTitle}`;
-  }, [onTitle]);
+
+  // useEffect(() => {
+  //   const prevTitle = document.title;
+  //   document.title = `Appmawa | ${onTitle}`;
+  //   return () => {
+  //     document.title = prevTitle;
+  //   };
+  // }, [onTitle]);
+
+  // Using useTitle function of react-use
+  useTitle(`Appmawa | ${onTitle}`);
 
   return <>{children}</>;
 };
